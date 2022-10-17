8304257a974cc25eed65c24fea941680bb78c8ad
Enzo Magal <61931855+enmagal@users.noreply.github.com>
2022-10-17 15:03:24 +0200

    Add files via upload

---
+++ tp_1/tp_1/build/CMakeFiles/tp1_test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/src/cartesien.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/src/cartesien.cpp.o"
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/src/nuage.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/src/nuage.cpp.o"
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/src/point.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/src/point.cpp.o"
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/src/polaire.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/src/polaire.cpp.o"
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/test/catch.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/test/catch.cpp.o"
+  "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/test/tp1_test.cpp" "/home/local.isima.fr/enmagal/TP_Cpp_ZZ3/tp_1/build/CMakeFiles/tp1_test.dir/test/tp1_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tp_1/tp_1/src/polaire.cpp
@@ -0,0 +1,60 @@
+#include "polaire.hpp"
+#include "cartesien.hpp"
+
+#include <sstream>
+#include <cmath>
+
+Polaire::Polaire() : a(0), d(0)
+{
+}
+
+Polaire::Polaire(double pa, double pd) : a(pa), d(pd)
+{
+}
+
+Polaire::Polaire(Cartesien c)
+{
+    a = std::atan2(c.getY(), c.getX()) * 180 / (2 * acos(0.0));
+    d = sqrt(c.getX()*c.getX() + c.getY()*c.getY());
+}
+
+Polaire::~Polaire()
+{
+}
+
+double Polaire::getAngle() const
+{
+    return a;
+}
+
+double Polaire::getDistance() const
+{
+    return d;
+}
+
+void Polaire::setAngle(double pa)
+{
+    a = pa;
+}
+
+void Polaire::setDistance(double pd)
+{
+    d = pd;
+}
+
+void Polaire::afficher(std::ostream& flux) const
+{
+    flux << "(a=" << getAngle() <<";d=" << getDistance() << ")";
+}
+
+void Polaire::convertir(Polaire& point_pol) const
+{
+    point_pol.setAngle(getAngle());
+    point_pol.setDistance(getDistance());
+}
+
+void Polaire::convertir(Cartesien& point_car) const
+{
+    point_car.setX(getDistance() * std::cos(getAngle() * (2 * acos(0.0)) / 180));
+    point_car.setY(getDistance() * std::sin(getAngle() * (2 * acos(0.0)) / 180));
+}
